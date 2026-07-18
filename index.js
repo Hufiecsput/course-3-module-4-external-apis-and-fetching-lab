149cb9bf164128c5b9d1a51ee62b144ea1696cf5
Hufiec <tevinfelix10@gmail.com>
2026-07-19 00:19:53 +0300

    selecting the html elemets

--- index.js
+++ index.js
@@ -2,3 +2,8 @@
 const weatherApi = "https://api.weather.gov/alerts/active?area="
 
 // Your code here!
+
+const input = document.getElementById("state-input");
+const button = document.getElementById("fetch -alerts");
+const display = document.getElementById("alert-display");
+const error = document.getElementById("error-message");
